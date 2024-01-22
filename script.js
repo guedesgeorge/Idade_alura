bd79d32fcbe190c36031c16bac442d422024dce9
´´George <george.emannuelcarvalho@gmail.com>
2024-01-22 20:13:13 -0300

    teste logica

---
+++ script.js
@@ -0,0 +1,13 @@
+  alert(`Qual é  a sua idade`);
+  let idade;
+
+  idade = prompt('Qual é a sua idade?');
+
+  let resultado = idade >= 18
+
+  if (resultado){
+    alert('Você é maior de idade');
+
+  }else{
+    alert('Você é menor de idade');
+  }
