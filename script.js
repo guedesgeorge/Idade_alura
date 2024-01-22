b79cff6296fb77adbd0d07c94de92ac62747e04c
´´George <george.emannuelcarvalho@gmail.com>
2024-01-22 20:14:52 -0300

    teste logica

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-  alert(`Qual é  a sua idade`);
-  let idade;
+    alert(`Qual é  a sua idade`);
+    let idade;
 
-  idade = prompt('Qual é a sua idade?');
+    idade = prompt('Qual é a sua idade?');
 
-  let resultado = idade >= 18
+    let resultado = idade >= 18
 
-  if (resultado){
-    alert('Você é maior de idade');
+    if (resultado){
+      alert('Você é maior de idade');
 
-  }else{
-    alert('Você é menor de idade');
-  }
+    }else{
+      alert('Você é menor de idade');
+    }
